2b4da16633eb9024750ea7bbae8e489ce2524880
yingqichao <shinydotcom@163.com>
2019-07-05 17:57:57 +0800

    7/5

--- src/BlockGraph.java
+++ src/BlockGraph.java
@@ -3,18 +3,18 @@ import java.util.Map;
 import java.util.Set;
 
 public class BlockGraph {
-    //Graph on which we run Belief Propagation to resolve source node data
-    public int num_blocks;
-    public Map<Integer,Integer> checks = new HashMap<>();
-    public Map<Integer,Integer> eliminated = new HashMap<>();
-
-    public BlockGraph(int num_blocks) {
-        this.num_blocks = num_blocks;
-    }
-
-    public boolean add_block(Set<Integer> nodes, Integer data){
-        if(nodes.size()==1){
-
-        }
-    }
+//    //Graph on which we run Belief Propagation to resolve source node data
+//    public int num_blocks;
+//    public Map<Integer,Integer> checks = new HashMap<>();
+//    public Map<Integer,Integer> eliminated = new HashMap<>();
+//
+//    public BlockGraph(int num_blocks) {
+//        this.num_blocks = num_blocks;
+//    }
+//
+//    public boolean add_block(Set<Integer> nodes, Integer data){
+//        if(nodes.size()==1){
+//
+//        }
+//    }
 }
